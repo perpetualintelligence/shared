2e06b907f2139dd9b605b0a2e1ae6401bcdc8667
Amey Subhedar <ameyasubhedar@gmail.com>
2021-11-28 13:53:10 -0800

    renamr

---
+++ src/PerpetualIntelligence.Test/OneImlxTestIdAttribute.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Copyright (c) 2019-2022. All Rights Reserved. Perpetual Intelligence L.L.C.
+    https://perpetualintelligence.com
+    https://api.perpetualintelligence.com
+*/
+
+using System;
+
+namespace PerpetualIntelligence.Test
+{
+    /// <summary>
+    /// Represents a unique <c>OneImlx</c> test id.
+    /// </summary>
+    public sealed class OneImlxTestIdAttribute : Attribute
+    {
+        /// <summary>
+        /// Initialize a new instance.
+        /// </summary>
+        /// <param name="id">The test id.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public OneImlxTestIdAttribute(string id)
+        {
+            Id = id ?? throw new ArgumentNullException(nameof(id));
+        }
+
+        /// <summary>
+        /// The test id.
+        /// </summary>
+        public string Id { get; set; };
+    }
+}
