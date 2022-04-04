10752467599a4b70e230c4e40956a75bd8f78134
Amey Subhedar <ameyasubhedar@gmail.com>
2022-04-04 16:48:36 -0700

    saas usage

---
+++ src/PerpetualIntelligence.Protocols/Licensing/SaaSUsage.cs
@@ -0,0 +1,30 @@
+﻿/*
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
+*/
+
+namespace PerpetualIntelligence.Protocols.Licensing
+{
+    /// <summary>
+    /// The SaaS usage.
+    /// </summary>
+    public class SaaSUsage
+    {
+        /// <summary>
+        /// The community SaaS pricing plan.
+        /// </summary>
+        public const string Commercial = "urn:oneimlx:lic:saasusage:commerce";
+
+        /// <summary>
+        /// The community SaaS pricing plan.
+        /// </summary>
+        public const string Educational = "urn:oneimlx:lic:saasusage:edu";
+
+        /// <summary>
+        /// The community SaaS pricing plan.
+        /// </summary>
+        public const string Test = "urn:oneimlx:lic:saasusage:test";
+    }
+}
