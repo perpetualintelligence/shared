549d371778e20ca4eebcefac9694e43a4eef9146
Amey Subhedar <ameyasubhedar@gmail.com>
2022-04-12 10:42:21 -0700

    key source

---
+++ src/PerpetualIntelligence.Protocols/Licensing/SaaSKeySources.cs
@@ -0,0 +1,39 @@
+﻿/*
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
+*/
+
+namespace PerpetualIntelligence.Protocols.Licensing
+{
+    /// <summary>
+    /// The SaaS key sources.
+    /// </summary>
+    public sealed class SaaSKeySources
+    {
+        /// <summary>
+        /// Determines if the key source is valid.
+        /// </summary>
+        /// <returns><c>true</c> if the key source is valid; otherwise, <c>false</c>.</returns>
+        public static bool IsValid(string keySource)
+        {
+            switch (keySource)
+            {
+                case JsonFile:
+                    {
+                        return true;
+                    }
+                default:
+                    {
+                        return false;
+                    }
+            }
+        }
+
+        /// <summary>
+        /// The key source is a json file.
+        /// </summary>
+        public const string JsonFile = "urn:oneimlx:lic:ksource:jsonfile";
+    }
+}
---
+++ test/PerpetualIntelligence.Protocols.Test/Licensing/SaaSKeySourcesTests.cs
@@ -0,0 +1,36 @@
+﻿/*
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
+*/
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PerpetualIntelligence.Test.Services;
+
+namespace PerpetualIntelligence.Protocols.Licensing
+{
+    [TestClass]
+    public class SaaSKeySourcesTests
+    {
+        [TestMethod]
+        public void IsValidShouldReturnFalseForInvalidSource()
+        {
+            Assert.IsFalse(SaaSKeySources.IsValid("invalid"));
+        }
+
+        [TestMethod]
+        public void IsValidShouldReturnTrueForValidUsage()
+        {
+            Assert.IsTrue(SaaSKeySources.IsValid(SaaSKeySources.JsonFile));
+        }
+
+        [TestMethod]
+        public void SaaSUsagesTest()
+        {
+            TestHelper.AssertConstantCount(typeof(SaaSKeySources), 1);
+
+            Assert.AreEqual("urn:oneimlx:lic:ksource:jsonfile", SaaSKeySources.JsonFile);
+        }
+    }
+}
