282ed759f796b261a8eb2ff2df4a2bba97157d9f
Amey Subhedar <ameyasubhedar@gmail.com>
2022-04-13 19:56:16 -0700

    base address

---
+++ src/PerpetualIntelligence.Protocols/Http/WellKnownBaseAddress.cs
@@ -0,0 +1,39 @@
+﻿/*
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
+*/
+
+using System;
+
+namespace PerpetualIntelligence.Protocols.Http
+{
+    /// <summary>
+    /// Defines a well known base address with associated HTTP client name.
+    /// </summary>
+    public class WellKnownBaseAddress
+    {
+        /// <summary>
+        /// Initialize a new instance.
+        /// </summary>
+        /// <param name="name">The HTTP client name.</param>
+        /// <param name="baseAddress">The base address.</param>
+        /// <exception cref="ArgumentNullException"></exception>
+        public WellKnownBaseAddress(string name, string baseAddress)
+        {
+            BaseAddress = baseAddress ?? throw new ArgumentNullException(nameof(baseAddress));
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        /// <summary>
+        /// The base address.
+        /// </summary>
+        public string BaseAddress { get; set; }
+
+        /// <summary>
+        /// The HTTP client name.
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
---
+++ src/PerpetualIntelligence.Protocols/Licensing/Models/LicenseProvisioningModel.cs
@@ -0,0 +1,79 @@
+﻿/*
+    Copyright (c) Perpetual Intelligence L.L.C. All Rights Reserved.
+
+    For license, terms, and data policies, go to:
+    https://terms.perpetualintelligence.com
+*/
+
+using System;
+using System.Text.Json.Serialization;
+
+namespace PerpetualIntelligence.Protocols.Licensing.Models
+{
+    /// <summary>
+    /// The license provisioning model.
+    /// </summary>
+    public class LicenseProvisioningModel
+    {
+        /// <summary>
+        /// The <c>acr</c> claim.
+        /// </summary>
+        [JsonPropertyName("acr_values")]
+        public string[] AcrValues { get; set; } = null!;
+
+        /// <summary>
+        /// The <c>auth_apps</c> claim.
+        /// </summary>
+        [JsonPropertyName("authorized_applications")]
+        public string[] AuthorizedApplications { get; set; } = null!;
+
+        /// <summary>
+        /// The <c>azp</c> claim.
+        /// </summary>
+        [JsonPropertyName("authorized_party")]
+        public string AuthorizedParty { get; set; } = null!;
+
+        /// <summary>
+        /// The consumer tenant id. This is the <c>tid</c> claim.
+        /// </summary>
+        [JsonPropertyName("consumer_tenant_country")]
+        public string? ConsumerTenantCountry { get; set; }
+
+        /// <summary>
+        /// The consumer tenant id. This is the <c>tid</c> claim.
+        /// </summary>
+        [JsonPropertyName("consumer_tenant_id")]
+        public string ConsumerTenantId { get; set; } = null!;
+
+        /// <summary>
+        /// The consumer tenant id. This is the <c>tid</c> claim.
+        /// </summary>
+        [JsonPropertyName("consumer_tenant_name")]
+        public string? ConsumerTenantName { get; set; }
+
+        /// <summary>
+        /// The jws key expiry.
+        /// </summary>
+        [JsonPropertyName("expiry")]
+        public DateTimeOffset Expiry { get; set; }
+
+        /// <summary>
+        /// The space separated keys operations. Valid value is <c>actions</c>, <c>get</c>, <c>generate</c>,
+        /// <c>reset_primary</c>, <c>reset_secondary</c> and <c>delete</c>.
+        /// </summary>
+        [JsonPropertyName("operation")]
+        public string Operation { get; set; } = null!;
+
+        /// <summary>
+        /// The provider tenant id.
+        /// </summary>
+        [JsonPropertyName("provider_tenant_id")]
+        public string ProviderTenantId { get; set; } = null!;
+
+        /// <summary>
+        /// The <c>sub</c> claim.
+        /// </summary>
+        [JsonPropertyName("subject")]
+        public string Subject { get; set; } = null!;
+    }
+}
