0609f2c4ea722c2703e20fc6328c053f6c9d483e
Amey Subhedar <ameyasubhedar@gmail.com>
2022-04-27 11:46:57 -0700

    update description

--- src/PerpetualIntelligence.Shared/Infrastructure/LoggingOptions.cs
+++ src/PerpetualIntelligence.Shared/Infrastructure/LoggingOptions.cs
@@ -8,18 +8,18 @@
 namespace PerpetualIntelligence.Shared.Infrastructure
 {
     /// <summary>
-    /// The generic logging configuration options.
+    /// The logging configuration options.
     /// </summary>
     public class LoggingOptions
     {
         /// <summary>
-        /// The string used to obscure error arguments. Defaults to <c>****</c>.
+        /// The string used to obscure error description arguments. The default value is <c>****</c>.
         /// </summary>
         /// <seealso cref="ObsureErrorArguments"/>
         public string ObscureErrorArgumentString { get; set; } = "****";
 
         /// <summary>
-        /// Obscures the arguments in the error description to hide the sensitive data. Defaults to <c>true</c>.
+        /// Obscures the arguments in the error description to hide the sensitive data. The default value is <c>true</c>.
         /// </summary>
         /// <remarks>Use <see cref="ObscureErrorArgumentString"/> to configure the obscure string.</remarks>
         /// <seealso cref="ObscureErrorArgumentString"/>
