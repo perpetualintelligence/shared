de74e5248152b415b1150f0f89c299a85b9e9c89
prawasu <dev@perpetualintelligence.com>
2023-12-19 19:16:10 -0800

    Fix assertion

--- src/OneImlx.Test/FluentAssertions/ErrorExceptionAssertionsExtensions.cs
+++ src/OneImlx.Test/FluentAssertions/ErrorExceptionAssertionsExtensions.cs
@@ -8,6 +8,7 @@
 using FluentAssertions.Execution;
 using FluentAssertions.Specialized;
 using OneImlx.Shared.Infrastructure;
+using System.Threading.Tasks;
 
 namespace OneImlx.Test.FluentAssertions
 {
@@ -17,48 +18,72 @@ namespace OneImlx.Test.FluentAssertions
     public static class ErrorExceptionAssertionsExtensions
     {
         /// <summary>
-        /// Asserts that a thrown <see cref="ErrorException"/> has a specific error.
-        /// </summary>
-        /// <param name="assertions">The wrapped <see cref="ExceptionAssertions{T}"/>.</param>
-        /// <param name="expectedError">The expected error to assert.</param>
-        /// <returns>The same <see cref="ExceptionAssertions{T}"/> to support fluent assertions chaining.</returns>
-        public static ExceptionAssertions<ErrorException> WithError(this ExceptionAssertions<ErrorException> assertions, Error expectedError)
-        {
-            Execute.Assertion
-                .ForCondition(assertions.Which.Error == expectedError)
-                .FailWith("Expected error {0}, but found {1}.", expectedError, assertions.Which.Error.ErrorCode);
-
-            return assertions;
-        }
-
-        /// <summary>
         /// Asserts that a thrown <see cref="ErrorException"/> has a specific error code.
+        /// This method should be used after an asynchronous operation that is expected to throw an exception.
         /// </summary>
-        /// <param name="assertions">The wrapped <see cref="ExceptionAssertions{T}"/>.</param>
-        /// <param name="expectedErrorCode">The expected error code to assert.</param>
-        /// <returns>The same <see cref="ExceptionAssertions{T}"/> to support fluent assertions chaining.</returns>
-        public static ExceptionAssertions<ErrorException> WithErrorCode(this ExceptionAssertions<ErrorException> assertions, string expectedErrorCode)
+        /// <param name="task">The task resulting from the asynchronous operation.</param>
+        /// <param name="expectedErrorCode">The expected error code.</param>
+        /// <param name="because">A formatted phrase explaining why the assertion should be satisfied.</param>
+        /// <param name="becauseArgs">Zero or more values to use for filling in any gaps in the <paramref name="because"/> phrase.</param>
+        /// <returns>A continuation object for method chaining.</returns>
+        public static async Task<ExceptionAssertions<TException>> WithErrorCode<TException>(
+            this Task<ExceptionAssertions<TException>> task, string expectedErrorCode, string because = "", params object[] becauseArgs)
+            where TException : ErrorException
         {
-            Execute.Assertion
-                .ForCondition(assertions.Which.Error.ErrorCode == expectedErrorCode)
-                .FailWith("Expected error code {0}, but found {1}.", expectedErrorCode, assertions.Which.Error.ErrorCode);
+            var exceptionAssertions = await task;
 
-            return assertions;
+            Execute.Assertion
+                .ForCondition(exceptionAssertions.Which.Error.ErrorCode == expectedErrorCode)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected error code to be {0}{reason}, but found {1}.", expectedErrorCode, exceptionAssertions.Which.Error.ErrorCode);
+
+            return exceptionAssertions;
         }
 
         /// <summary>
         /// Asserts that a thrown <see cref="ErrorException"/> has a specific error description.
+        /// This method should be used after an asynchronous operation that is expected to throw an exception.
         /// </summary>
-        /// <param name="assertions">The wrapped <see cref="ExceptionAssertions{T}"/>.</param>
-        /// <param name="expectedErrorDescription">The expected error description to assert.</param>
-        /// <returns>The same <see cref="ExceptionAssertions{T}"/> to support fluent assertions chaining.</returns>
-        public static ExceptionAssertions<ErrorException> WithErrorDescription(this ExceptionAssertions<ErrorException> assertions, string expectedErrorDescription)
+        /// <param name="task">The task resulting from the asynchronous operation.</param>
+        /// <param name="expectedErrorDescription">The expected error description.</param>
+        /// <param name="because">A formatted phrase explaining why the assertion should be satisfied.</param>
+        /// <param name="becauseArgs">Zero or more values to use for filling in any gaps in the <paramref name="because"/> phrase.</param>
+        /// <returns>A continuation object for method chaining.</returns>
+        public static async Task<ExceptionAssertions<TException>> WithErrorDescription<TException>(
+            this Task<ExceptionAssertions<TException>> task, string expectedErrorDescription, string because = "", params object[] becauseArgs)
+            where TException : ErrorException
         {
-            Execute.Assertion
-                .ForCondition(assertions.Which.Error.ErrorDescription == expectedErrorDescription)
-                .FailWith("Expected error description {0}, but found {1}.", expectedErrorDescription, assertions.Which.Error.ErrorDescription);
+            var exceptionAssertions = await task;
 
-            return assertions;
+            Execute.Assertion
+                .ForCondition(exceptionAssertions.Which.Error.FormatDescription() == expectedErrorDescription)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected error description to be {0}{reason}, but found {1}.", expectedErrorDescription, exceptionAssertions.Which.Error.FormatDescription());
+
+            return exceptionAssertions;
+        }
+
+        /// <summary>
+        /// Asserts that a thrown <see cref="ErrorException"/> has a specific error description.
+        /// This method should be used after an asynchronous operation that is expected to throw an exception.
+        /// </summary>
+        /// <param name="task">The task resulting from the asynchronous operation.</param>
+        /// <param name="expectedError">The expected error.</param>
+        /// <param name="because">A formatted phrase explaining why the assertion should be satisfied.</param>
+        /// <param name="becauseArgs">Zero or more values to use for filling in any gaps in the <paramref name="because"/> phrase.</param>
+        /// <returns>A continuation object for method chaining.</returns>
+        public static async Task<ExceptionAssertions<TException>> WithError<TException>(
+            this Task<ExceptionAssertions<TException>> task, Error expectedError, string because = "", params object[] becauseArgs)
+            where TException : ErrorException
+        {
+            var exceptionAssertions = await task;
+
+            Execute.Assertion
+                .ForCondition(exceptionAssertions.Which.Error == expectedError)
+                .BecauseOf(because, becauseArgs)
+                .FailWith("Expected error to be {0}{reason}, but found {1}.", expectedError, exceptionAssertions.Which.Error);
+
+            return exceptionAssertions;
         }
     }
 }
