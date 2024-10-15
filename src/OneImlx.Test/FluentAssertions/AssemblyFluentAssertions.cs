853e1ec8f3315a9e1ba081fb7b14174ddb01dc1c
pi.admin <orgadmin@perpetualintelligence.com>
2024-10-14 19:21:49 -0700

    Fix gRPC generated types location test issue

--- src/OneImlx.Test/FluentAssertions/AssemblyFluentAssertions.cs
+++ src/OneImlx.Test/FluentAssertions/AssemblyFluentAssertions.cs
@@ -1,20 +1,20 @@
 ﻿/*
-    Copyright (c) 2023 Perpetual Intelligence L.L.C. All Rights Reserved.
+    Copyright © 2019-2025 Perpetual Intelligence L.L.C. All rights reserved.
 
     For license, terms, and data policies, go to:
     https://terms.perpetualintelligence.com/articles/intro.html
 */
 
-using FluentAssertions;
-using FluentAssertions.Execution;
-using FluentAssertions.Reflection;
-using OneImlx.Shared.Infrastructure;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using FluentAssertions.Reflection;
+using OneImlx.Shared.Infrastructure;
 
 namespace OneImlx.Test.FluentAssertions
 {
@@ -24,10 +24,53 @@ namespace OneImlx.Test.FluentAssertions
     public static class AssemblyFluentAssertions
     {
         /// <summary>
+        /// Asserts that the assembly has a valid namespace based on its name.
+        /// </summary>
+        /// <param name="assertions">Fluent Assertions wrapper for the assembly.</param>
+        /// <param name="rootNamespace">The expected root namespace.</param>
+        public static AndConstraint<AssemblyAssertions> HaveTypesInRootNamespace(this AssemblyAssertions assertions, string rootNamespace)
+        {
+            var assembly = assertions.Subject;
+            var actualNamespace = assembly.GetName().Name ?? throw new InvalidOperationException("Assembly name null");
+
+            Execute.Assertion
+                .ForCondition(actualNamespace == rootNamespace)
+                .FailWith($"Assembly '{assembly.GetName().Name}' does not have root namespace '{rootNamespace}'.");
+
+            var types = assembly.GetTypes().Where(e => !IsCompilerGenerated(e));
+            var invalidTypes = types.Where(e =>
+            {
+                // Either the namespace should match root namespace or it should be nested namespace. This is to avoid
+                if (e.Namespace != null && e.Namespace.StartsWith(rootNamespace))
+                {
+                    // Make sure do not have any concatenated names to the root namespace without nesting. e.g.
+                    // OneImlx.Shared123 - invalid OneImlx.Shared.Services - valid OneImlx.Shared - valid
+                    string trimmed = e.Namespace.Replace(rootNamespace, "");
+                    if (trimmed == string.Empty || trimmed.StartsWith("."))
+                    {
+                        // keep
+                        return false;
+                    }
+                }
+
+                // by default filter
+                return true;
+            });
+
+            if (invalidTypes.Any())
+            {
+                throw new AssertionFailedException($"Assembly '{assembly.GetName().Name}' contains types '{string.Join(",", invalidTypes.Select(e => e.FullName))}' that do not have root namespace '{rootNamespace}'.");
+            }
+
+            return new AndConstraint<AssemblyAssertions>(assertions);
+        }
+
+        /// <summary>
         /// Asserts that the assembly has all types in the valid files or locations.
         /// </summary>
         /// <param name="assertions">Fluent Assertions wrapper for the assembly.</param>
-        public static AndConstraint<AssemblyAssertions> HaveTypesInValidLocations(this AssemblyAssertions assertions)
+        /// <param name="excludeTypes">The types to exclude checks.</param>
+        public static AndConstraint<AssemblyAssertions> HaveTypesInValidLocations(this AssemblyAssertions assertions, IEnumerable<Type>? excludeTypes = null)
         {
             var assembly = assertions.Subject;
 
@@ -60,50 +103,7 @@ namespace OneImlx.Test.FluentAssertions
 
                 var nsTypes = assembly.GetTypes().Where(e => ns.Equals(e.Namespace) && !IsCompilerGenerated(e));
                 ns.Should().NotBeNull("Namespace cannot be null or empty.");
-                AssertNamespaceTypesLocation(nsTypes, ns, srcDir);
-            }
-
-            return new AndConstraint<AssemblyAssertions>(assertions);
-        }
-
-        /// <summary>
-        /// Asserts that the assembly has a valid namespace based on its name.
-        /// </summary>
-        /// <param name="assertions">Fluent Assertions wrapper for the assembly.</param>
-        /// <param name="rootNamespace">The expected root namespace.</param>
-        public static AndConstraint<AssemblyAssertions> HaveTypesInRootNamespace(this AssemblyAssertions assertions, string rootNamespace)
-        {
-            var assembly = assertions.Subject;
-            var actualNamespace = assembly.GetName().Name ?? throw new InvalidOperationException("Assembly name null");
-
-            Execute.Assertion
-                .ForCondition(actualNamespace == rootNamespace)
-                .FailWith($"Assembly '{assembly.GetName().Name}' does not have root namespace '{rootNamespace}'.");
-
-            var types = assembly.GetTypes().Where(e => !IsCompilerGenerated(e));
-            var invalidTypes = types.Where(e =>
-            {
-                // Either the namespace should match root namespace or it should be nested namespace. This is to avoid
-                if (e.Namespace != null && e.Namespace.StartsWith(rootNamespace))
-                {
-                    // Make sure do not have any concatenated names to the root namespace without nesting. e.g.
-                    // OneImlx.Shared123 - invalid OneImlx.Shared.Services - valid
-                    // OneImlx.Shared - valid
-                    string trimmed = e.Namespace.Replace(rootNamespace, "");
-                    if (trimmed == string.Empty || trimmed.StartsWith("."))
-                    {
-                        // keep
-                        return false;
-                    }
-                }
-
-                // by default filter
-                return true;
-            });
-
-            if (invalidTypes.Any())
-            {
-                throw new AssertionFailedException($"Assembly '{assembly.GetName().Name}' contains types '{string.Join(",", invalidTypes.Select(e => e.FullName))}' that do not have root namespace '{rootNamespace}'.");
+                AssertNamespaceTypesLocation(nsTypes, ns, srcDir, excludeTypes);
             }
 
             return new AndConstraint<AssemblyAssertions>(assertions);
@@ -115,7 +115,8 @@ namespace OneImlx.Test.FluentAssertions
         /// <param name="types">Types to check. All types must be in same assembly.</param>
         /// <param name="namespace">Namespace to check.</param>
         /// <param name="srcDir">src directory.</param>
-        private static void AssertNamespaceTypesLocation(IEnumerable<Type> types, string @namespace, string srcDir)
+        /// <param name="excludeTypes">The types to exclude checks.</param>
+        private static void AssertNamespaceTypesLocation(IEnumerable<Type> types, string @namespace, string srcDir, IEnumerable<Type>? excludeTypes = null)
         {
             // Check for same assembly.
             Assembly assembly = types.First().Assembly;
@@ -146,9 +147,9 @@ namespace OneImlx.Test.FluentAssertions
                 throw new ErrorException("internal_error", $"The namespace '{@namespace}' is not valid. The namespace component '{nonRootNamespace}' must be a directory.");
             }
 
-            // Make sure the type has the corresponding file.
-            // We cannot get the actual code file that defines the type. So we are just checking whether there is a new file for each type and file name matches type.
-            // Move this to an analyzer.
+            // Make sure the type has the corresponding file. We cannot get the actual code file that defines the type.
+            // So we are just checking whether there is a new file for each type and file name matches type. Move this
+            // to an analyzer.
             IEnumerable<string>? files = Directory.EnumerateFiles(namespaceDir, "*.cs");
             foreach (Type type in types)
             {
@@ -160,6 +161,12 @@ namespace OneImlx.Test.FluentAssertions
 
                 // We dont have any control on compiler generated types
                 if (IsCompilerGenerated(type))
+                {
+                    continue;
+                }
+
+                // Exclude types
+                if (excludeTypes != null && excludeTypes.Contains(type))
                 {
                     continue;
                 }
@@ -189,10 +196,42 @@ namespace OneImlx.Test.FluentAssertions
             }
 
             // Make sure the types count matches the files count
-            if (files.Count() != types.Count())
+            int expectedCount = types.Except(excludeTypes).Count();
+            if (files.Count() != expectedCount)
             {
-                throw new ErrorException("internal_error", $"Namespace '{@namespace}' has {types.Count()} types, but it has {files.Count()} source files.");
+                throw new ErrorException("internal_error", $"Namespace '{@namespace}' has {expectedCount} types, but it has {files.Count()} source files.");
             }
+        }
+
+        private static string GetAssemblyPath(Assembly assembly)
+        {
+            // Attempt to use CodeBase for full .NET Framework compatibility Fallback to Location for .NET Core and later
+            string location;
+            try
+            {
+                // CodeBase is marked obsolete in .NET 5.0 and later versions, but still usable in earlier versions
+                location = new Uri(assembly.CodeBase).LocalPath;
+            }
+            catch (Exception)
+            {
+                // If accessing CodeBase fails or it's not applicable, use Location
+                location = assembly.Location;
+            }
+
+            return location;
+        }
+
+        private static string GetRootPathBasedOnAssemblyLocation(string assemblyPath)
+        {
+            // Go up six levels from the assembly location to get to the project root.
+            var directory = new DirectoryInfo(assemblyPath);
+            for (int i = 0; i < 6; i++)
+            {
+                directory = directory?.Parent;
+                if (directory == null) throw new InvalidOperationException("Failed to calculate the project root path.");
+            }
+
+            return directory.FullName;
         }
 
         private static bool IsCompilerGenerated(Type type)
@@ -218,6 +257,11 @@ namespace OneImlx.Test.FluentAssertions
                 return true;
             }
 
+            if (type.Name.Contains("`"))
+            {
+                return true;
+            }
+
             return false;
         }
 
@@ -229,38 +273,6 @@ namespace OneImlx.Test.FluentAssertions
         private static bool IsDelegate(Type type)
         {
             return type.IsSubclassOf(typeof(Delegate));
-        }
-
-        private static string GetRootPathBasedOnAssemblyLocation(string assemblyPath)
-        {
-            // Go up six levels from the assembly location to get to the project root.
-            var directory = new DirectoryInfo(assemblyPath);
-            for (int i = 0; i < 6; i++)
-            {
-                directory = directory?.Parent;
-                if (directory == null) throw new InvalidOperationException("Failed to calculate the project root path.");
-            }
-
-            return directory.FullName;
-        }
-
-        private static string GetAssemblyPath(Assembly assembly)
-        {
-            // Attempt to use CodeBase for full .NET Framework compatibility
-            // Fallback to Location for .NET Core and later
-            string location;
-            try
-            {
-                // CodeBase is marked obsolete in .NET 5.0 and later versions, but still usable in earlier versions
-                location = new Uri(assembly.CodeBase).LocalPath;
-            }
-            catch (Exception)
-            {
-                // If accessing CodeBase fails or it's not applicable, use Location
-                location = assembly.Location;
-            }
-
-            return location;
         }
     }
 }
